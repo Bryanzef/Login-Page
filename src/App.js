8a5329e3fb8cff8c3d2f8bb1c5c87a7d8cc2f95e
Bryanzef <bryanzef7@gmail.com>
2023-04-01 09:29:34 -0300

    First commit

---
+++ src/App.js
@@ -0,0 +1,46 @@
+import React, { useState } from "react";
+import "./Login.css";
+
+const Login = () => {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState("");
+
+  const handleLogin = (event) => {
+    event.preventDefault();
+    if (username === "admin" && password === "admin") {
+      alert("Login successful");
+    } else {
+      setError("Incorrect username or password");
+    }
+  };
+  return (
+    <div className="login-container">
+      <h1>Login</h1>
+      <form onSubmit={handleLogin}>
+        <div className="form-group">
+          <label htmlFor="username">Username:</label>
+          <input
+            type="text"
+            id="username"
+            value={username}
+            onChange={(event) => setUsername(event.target.value)}
+          />
+        </div>
+        <div className="form-group">
+          <label htmlFor="password">Password:</label>
+          <input
+            type="password"
+            id="password"
+            value={password}
+            onChange={(event) => setPassword(event.target.value)}
+          />
+        </div>
+        {error && <div className="error">{error}</div>}
+        <button type="submit">Login</button>
+      </form>
+    </div>
+  );
+};
+
+export default Login;
